f82cc0129b8fad97317a6621c9bdec1cb4dce22e
Zarah Sikora <zarahsikora1@Zarahs-MacBook-Pro.local>
2017-11-21 17:32:41 -0500

    Add config folder

---
+++ wp-config/wp-db-staging.php
@@ -0,0 +1,12 @@
+<?php
+
+if (!defined('ABSPATH')) exit();
+
+define('DB_NAME', 'preserve'); 
+define('DB_USER', 'devontriley'); 
+define('DB_PASSWORD', ''); 
+define('DB_HOST', ''); 
+define('DB_CHARSET', 'utf8'); 
+define('DB_COLLATE', '');
+
+ ?>
