49bee5999f713121167927f7cce4439826187233
Zarah Sikora <zarahsikora1@Zarahs-MacBook-Pro.local>
2017-11-27 10:04:11 -0500

    Adjust config for local

--- wp-config/wp-db-local.php
+++ wp-config/wp-db-local.php
@@ -2,11 +2,11 @@
 
 if (!defined('ABSPATH')) exit();
 
-define('DB_NAME', 'preserve'); 
-define('DB_USER', 'root'); 
-define('DB_PASSWORD', 'root'); 
-define('DB_HOST', 'localhost'); 
-define('DB_CHARSET', 'utf8'); 
+define('DB_NAME', 'preserve2');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_HOST', 'localhost');
+define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
  ?>
