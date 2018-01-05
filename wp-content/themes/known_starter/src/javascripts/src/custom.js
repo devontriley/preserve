cae388beb6c67a14a89aa935404a6e69baf8a313
Devon Riley <devontriley@gmail.com>
2018-01-05 14:34:51 -0500

    woocommerce development

--- wp-content/themes/known_starter/src/javascripts/src/custom.js
+++ wp-content/themes/known_starter/src/javascripts/src/custom.js
@@ -43,6 +43,65 @@ if(shopCategories) {
   });
 }
 
+// Custom quantity input
+jQuery(document).ready(function(){
+    // This button will increment the value
+    $('[data-quantity="plus"]').click(function(e){
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        fieldName = $(this).attr('data-field');
+        // Get its current value
+        var currentVal = parseInt($('input[name='+fieldName+']').val());
+        // If is not undefined
+        if (!isNaN(currentVal)) {
+            // Increment
+            $('input[name='+fieldName+']').val(currentVal + 1);
+        } else {
+            // Otherwise put a 0 there
+            $('input[name='+fieldName+']').val(0);
+        }
+    });
+    // This button will decrement the value till 0
+    $('[data-quantity="minus"]').click(function(e) {
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        fieldName = $(this).attr('data-field');
+        // Get its current value
+        var currentVal = parseInt($('input[name='+fieldName+']').val());
+        // If it isn't undefined or its greater than 0
+        if (!isNaN(currentVal) && currentVal > 0) {
+            // Decrement one
+            $('input[name='+fieldName+']').val(currentVal - 1);
+        } else {
+            // Otherwise put a 0 there
+            $('input[name='+fieldName+']').val(0);
+        }
+    });
+});
+
+// Product Image Gallery
+// class productImageGallery {
+//   constructor() {
+//     var gallery = $('.woocommerce-product-gallery__wrapper'),
+//         thumbnails = $('.thumbnails-wrapper .woocommerce-product-gallery__image');
+//
+//     thumbnails.on('click', function(e){
+//       e.preventDefault();
+//
+//       var image = $(this).find('img'),
+//           imgPath = image..attr('data-')
+//     });
+//   }
+// }
+
+if($('.woocommerce-product-gallery__wrapper').length){
+  var prodGallery = new productImageGallery;
+}
+
+
+
 // CATEGORIES BAR
 
 var catBarActive = false,
---
+++ wp-content/themes/known_starter/src/woocommerce/global/quantity-input.php
@@ -0,0 +1,35 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( $max_value && $min_value === $max_value ) {
+	?>
+	<div class="quantity hidden">
+		<input type="hidden" id="<?php echo esc_attr( $input_id ); ?>" class="qty" name="<?php echo esc_attr( $input_name ); ?>" value="<?php echo esc_attr( $min_value ); ?>" />
+	</div>
+	<?php
+} else {
+	?>
+	<div class="quantity">
+		<!--
+    <label class="screen-reader-text" for="<?php echo esc_attr( $input_id ); ?>"><?php esc_html_e( 'Quantity', 'woocommerce' ); ?></label>
+		<input type="number" id="<?php echo esc_attr( $input_id ); ?>" class="input-text qty text" step="<?php echo esc_attr( $step ); ?>" min="<?php echo esc_attr( $min_value ); ?>" max="<?php echo esc_attr( 0 < $max_value ? $max_value : '' ); ?>" name="<?php echo esc_attr( $input_name ); ?>" value="<?php echo esc_attr( $input_value ); ?>" title="<?php echo esc_attr_x( 'Qty', 'Product quantity input tooltip', 'woocommerce' ) ?>" size="4" pattern="<?php echo esc_attr( $pattern ); ?>" inputmode="<?php echo esc_attr( $inputmode ); ?>" />
+    -->
+    <div class="input-group plus-minus-input">
+      <div class="input-group-button">
+        <button type="button" class="button hollow circle" data-quantity="minus" data-field="quantity">
+          <i class="fa fa-minus" aria-hidden="true"></i>
+        </button>
+      </div>
+      <input id="<?php echo esc_attr( $input_id ); ?>" class="input-group-field input-text qty text" step="<?php echo esc_attr( $step ); ?>" min="<?php echo esc_attr( $min_value ); ?>" max="<?php echo esc_attr( 0 < $max_value ? $max_value : '' ); ?>" type="number" name="<?php echo esc_attr( $input_name ); ?>" value="<?php echo esc_attr( $input_value ); ?>" title="<?php echo esc_attr_x( 'Qty', 'Product quantity input tooltip', 'woocommerce' ) ?>" size="4">
+      <div class="input-group-button">
+        <button type="button" class="button hollow circle" data-quantity="plus" data-field="quantity">
+          <i class="fa fa-plus" aria-hidden="true"></i>
+        </button>
+      </div>
+    </div>
+	</div>
+	<?php
+}
