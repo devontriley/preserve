1c964a455967f96a8f2b9bfc5278a5f71a587b5b
Zarah Sikora <zarahsikora1@Zarahs-MacBook-Pro.local>
2018-01-15 11:02:13 -0500

    woops i forgot to save a few things

--- wp-content/themes/known_starter/src/page.php
+++ wp-content/themes/known_starter/src/page.php
@@ -1,11 +1,7 @@
 <?php get_header(); ?>
 
 <?php
-<<<<<<< HEAD
-  if( is_cart() || is_checkout() ) {
-=======
   if( is_cart() ) {
->>>>>>> cae388beb6c67a14a89aa935404a6e69baf8a313
     if(have_posts()) : while(have_posts()) : the_post();
 
       the_content();
