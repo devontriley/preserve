92142d08ee715ce67426260bbde92c5223d2b6bf
Devon Riley <devontriley@gmail.com>
2018-01-24 20:53:48 -0500

    add woo check

--- wp-content/themes/known_starter/src/page.php
+++ wp-content/themes/known_starter/src/page.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 
 <?php
+if(class_exists('woocommerce')){
   if( is_cart() ) {
     if(have_posts()) : while(have_posts()) : the_post();
 
@@ -8,6 +9,7 @@
 
     endwhile; endif;
   }
+}
 ?>
 
 <?php include('components.php'); ?>
