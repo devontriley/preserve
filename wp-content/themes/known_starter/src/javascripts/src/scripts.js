6d86cef6a5847da1084ee34935dd2f86c7c8288d
Devon Riley <devontriley@gmail.com>
2018-01-24 21:28:18 -0500

    fix

--- wp-content/themes/known_starter/src/javascripts/src/scripts.js
+++ wp-content/themes/known_starter/src/javascripts/src/scripts.js
@@ -186,9 +186,10 @@ function headerState()
 	// Display footer newsletter signup if the modal cookie is set
 	// This avoids conflicts with multiple mailchimp forms on one page
 	var form = '<div id="mc_embed_signup"><form action="//preservebrands.us16.list-manage.com/subscribe/post?u=7df2ba034c9d88245475dc567&amp;id=58c6176cc6" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate><div class="mc-field-group"><input type="email" value="" name="EMAIL" placeholder="example@email.com" class="required email" id="mce-EMAIL"></div><div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_7df2ba034c9d88245475dc567_58c6176cc6" tabindex="-1" value=""></div><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"><div id="mce-responses" class="clear"><div class="response" id="mce-error-response" style="display:none"></div><div class="response" id="mce-success-response" style="display:none"></div></div></form></div>';
-
+	$('#footer-newsletter-signup').append(form);
+	
 	if (document.cookie.replace(/(?:(?:^|.*;\s*)newsletterModal\s*\=\s*([^;]*).*$)|^.*$/, "$1") === "true") {
-		$('#footer-newsletter-signup').append(form);
+
 	} else {
 		// $('#newsletter-modal-container').append(form);
     //
