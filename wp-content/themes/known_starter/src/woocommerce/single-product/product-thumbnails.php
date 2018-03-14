1367d36373bd58d196b98d5d9f41c90bf15c1586
Devon Riley <devontriley@gmail.com>
2018-03-14 10:41:04 -0400

    bxslider styling

--- wp-content/themes/known_starter/src/woocommerce/single-product/product-thumbnails.php
+++ wp-content/themes/known_starter/src/woocommerce/single-product/product-thumbnails.php
@@ -26,7 +26,7 @@ if ( $attachment_ids && has_post_thumbnail() ) {
 			'data-large_image_height' => $full_size_image[2],
 		);
 
-		$html  = '<div data-thumb="' . esc_url( $thumbnail[0] ) . '" class="woocommerce-product-gallery__image '. ($i === 1) ? 'active' : '' .'">';
+		$html  = '<div data-thumb="' . esc_url( $thumbnail[0] ) . '" class="woocommerce-product-gallery__image '. ($i == 0) ? 'active' : '' .'">';
 		$html .= wp_get_attachment_image( $attachment_id, 'shop_single', false, $attributes );
  		$html .= '</div>';
 
