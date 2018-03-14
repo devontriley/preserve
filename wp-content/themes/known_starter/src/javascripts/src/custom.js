80daabf2961078b1a07b500f680d0108aeb8c546
Devon Riley <devontriley@gmail.com>
2018-03-14 11:02:42 -0400

    bxslider styling

--- wp-content/themes/known_starter/src/javascripts/src/custom.js
+++ wp-content/themes/known_starter/src/javascripts/src/custom.js
@@ -278,8 +278,7 @@ var mainSlider = productSlider.bxSlider({
         document.getElementById('gallery-bxslider').classList.remove('load-delay');
     },
     onSlideAfter: function(slide) {
-        console.log(slide);
-        changeActiveThumb(slide);
+        changeActiveThumb(slide[0]);
     }
 });
 
