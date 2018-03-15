22f5cc9b4a3ac5a84c0583fd37c3437071938f97
taroteeth <zarahsikora@gmail.com>
2018-03-15 16:54:37 -0400

    remove extra text from merge

--- wp-content/themes/known_starter/src/functions.php
+++ wp-content/themes/known_starter/src/functions.php
@@ -174,7 +174,6 @@ remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_singl
       $q->set( 'posts_per_page', -1 );
   }
 
-<<<<<<< HEAD
   //add project dimensions to product single page
   add_action( 'woocommerce_before_add_to_cart_form', 'show_dimensions', 9 );
 =======
