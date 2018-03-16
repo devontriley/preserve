cbdf7913da60395dfb9d13f851e552f7514ca717
taroteeth <zarahsikora@gmail.com>
2018-03-16 10:43:36 -0400

    rotate back to shop arrow

--- wp-content/themes/known_starter/src/components/back-to-shop.php
+++ wp-content/themes/known_starter/src/components/back-to-shop.php
@@ -1,9 +1,11 @@
 <div id="back-to-shop">
   <a href="<?php bloginfo('url'); ?>/shop">
-    <svg class="right-arrow" height="10" transform='rotate(180)' viewBox="0 0 5.41 10">
+    <svg class="right-arrow" height="10" viewBox="0 0 5.41 10">
       <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#right-arrow"></use>
     </svg>
 
     Back to Shop
   </a>
 </div>
+
+<!-- transform='rotate(180)' -->
