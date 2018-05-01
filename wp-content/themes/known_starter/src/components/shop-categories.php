359f06956050512b98de1803e70d2a929c5c6033
Devon Riley <devontriley@gmail.com>
2018-05-01 16:47:33 -0400

    Force show categories on shop

--- wp-content/themes/known_starter/src/components/shop-categories.php
+++ wp-content/themes/known_starter/src/components/shop-categories.php
@@ -2,10 +2,11 @@
 global $wp_query;
 
 $termCat = $wp_query->get_queried_object()->term_id;
-
 $args = array(
-  'taxonomy' => 'product_cat',
-  'title_li' => ''
+    'taxonomy' => 'product_cat',
+    'title_li' => '',
+    'hide_empty' => 0,
+    'exclude' => '26' // Exclude uncategorized
 );
 
 $queryCat = get_query_var('prod_cat');
@@ -13,23 +14,24 @@ $shopCategories = get_categories($args);
 
 if($shopCategories){
 
-  echo '<div class="page-nav-bar"><div class="inner">';
+    echo '<div class="page-nav-bar"><div class="inner">';
 
-  echo '<div><a href="'. get_bloginfo('url') .'/shop" class="'. (!$queryCat ? 'active' : '') .'" data-cat="0" data-slug="all">All</a></div>';
+    echo '<div><a href="'. get_bloginfo('url') .'/shop" class="'. (!$queryCat ? 'active' : '') .'" data-cat="0" data-slug="all">All</a></div>';
 
-  foreach($shopCategories as $c){
+    foreach($shopCategories as $c){
 
-    $active = false;
-    $catLink = get_term_link( $c->term_id );
-    if(($queryCat && $queryCat === $c->slug) || ($termCat === $c->term_id)) {
-      $active = true;
+        $active = false;
+        $catLink = get_term_link( $c->term_id );
+        
+        if(($queryCat && $queryCat === $c->slug) || ($termCat === $c->term_id)) {
+            $active = true;
+        }
+
+        echo '<div><a href="'. $catLink .'" class="'. ($active ? 'active' : '')  .'" data-cat="'. $c->term_id .'" data-slug="'. $c->slug .'">'. $c->name .'</a></div>';
+
     }
 
-    echo '<div><a href="'. $catLink .'" class="'. ($active ? 'active' : '')  .'" data-cat="'. $c->term_id .'" data-slug="'. $c->slug .'">'. $c->name .'</a></div>';
-
-  }
-
-  echo '</div></div><!-- .page-nav-bar -->';
+    echo '</div></div><!-- .page-nav-bar -->';
 
 }
 
--- wp-content/themes/known_starter/src/functions.php
+++ wp-content/themes/known_starter/src/functions.php
@@ -120,16 +120,9 @@ if( class_exists('woocommerce') ) :
  * Set your own value for 'posts_per_page'
  *
  */
-function woo_related_products_limit() {
-    global $product;
-
-    $args['posts_per_page'] = 3;
-    return $args;
-}
 add_filter( 'woocommerce_output_related_products_args', 'jk_related_products_args' );
 function jk_related_products_args( $args ) {
-    $args['posts_per_page'] = 3; // 4 related products
-    $args['columns'] = 1; // arranged in 2 columns
+    $args['posts_per_page'] = 3;
     return $args;
 }
 
